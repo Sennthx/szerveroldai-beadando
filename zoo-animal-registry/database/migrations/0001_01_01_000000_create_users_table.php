c4b35a4a91f16c93e9cd4f5d6c1cb7c18ea26a81
Sennthx <63876124+Sennthx@users.noreply.github.com>
2025-04-21 19:11:23 +0200

    Created migrations and table logic

--- zoo-animal-registry/database/migrations/0001_01_01_000000_create_users_table.php
+++ zoo-animal-registry/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean("admin")->default(false);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
+            
             $table->timestamps();
         });
 
