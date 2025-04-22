5bcb93e55e2fc55095172ac9a00eff716d8a89fe
Sennthx <63876124+Sennthx@users.noreply.github.com>
2025-04-23 00:18:37 +0200

    Refactor routes a bit, may change

--- zoo-animal-registry/routes/web.php
+++ zoo-animal-registry/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AnimalController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EnclosureController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
@@ -10,28 +9,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
 
-    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/', [HomeController::class, 'index'])
+        ->middleware(['auth', 'verified'])->name('home');
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
-
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('/', [HomeController::class, 'index'])->name('home');
 
-    Route::resource('enclosures', EnclosureController::class)->except(['create', 'store', 'edit', 'update', 'destroy']);
-    Route::resource('animals', AnimalController::class)->only(['index', 'show']);
+    Route::resource('enclosures', EnclosureController::class)
+        ->only(['index', 'show']);
+
+    Route::resource('animals', AnimalController::class)
+        ->only(['index']);
 
     // Admin-only routes
     Route::middleware(['admin'])->group(function () {
-        Route::resource('enclosures', EnclosureController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
-        Route::resource('animals', AnimalController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
-        Route::get('/archived-animals', [AnimalController::class, 'archived'])->name('animals.archived');
+
+        Route::resource('enclosures', EnclosureController::class)
+            ->only(['create', 'store', 'edit', 'update', 'destroy']);
+
+        Route::resource('animals', AnimalController::class)
+            ->only(['create', 'store', 'edit', 'update', 'destroy']);
+
+        // Archived animal functionality
+        Route::get('/animals/archived', [AnimalController::class, 'archived'])->name('animals.archived');
         Route::post('/animals/{animal}/restore', [AnimalController::class, 'restore'])->name('animals.restore');
     });
 });
