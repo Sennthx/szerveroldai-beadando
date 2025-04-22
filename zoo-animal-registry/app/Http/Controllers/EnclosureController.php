43644bfb0d8f5970ca7d3d199ad39180e35323f0
Sennthx <63876124+Sennthx@users.noreply.github.com>
2025-04-23 00:17:52 +0200

    Added index and show methods for enclosure and finished the main page

--- zoo-animal-registry/app/Http/Controllers/EnclosureController.php
+++ zoo-animal-registry/app/Http/Controllers/EnclosureController.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Animal;
+use App\Models\Enclosure;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class EnclosureController extends Controller
 {
     public function index()
     {
-        return view('enclosures.index');
+
+        $enclosures = new Collection();
+        if (Auth::user()->admin) {
+            $enclosures = Enclosure::orderBy('name')->get();
+        } else {
+            $enclosures = Auth::user()->enclosures
+                ->sortBy('name');
+        }
+
+        $enclosures = $enclosures->map(function ($enclosure) {
+            $enclosure->current_animals_count = Animal::where('enclosure_id', '=', $enclosure->id)->get()->count();
+            return $enclosure;
+        });
+
+        return view('enclosures.index', compact('enclosures'));
+    }
+
+    public function show($id)
+    {
+
+        $enclosure = Enclosure::find($id);
+
+        $animals = $enclosure->animals
+            ->sortBy([
+                ['species', 'asc'],
+                ['birth_date', 'asc'],
+            ]);
+
+
+        return view('enclosures.show', compact('enclosure', "animals"));
     }
 }
--- zoo-animal-registry/app/Http/Controllers/HomeController.php
+++ zoo-animal-registry/app/Http/Controllers/HomeController.php
@@ -6,34 +6,25 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Animal;
 use App\Models\Enclosure;
-use App\Models\User;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-
-        $title = config('app.name');
-        $enclosures = Auth::user()->enclosures;
-
         $enclosureCount = Enclosure::count();
         $animalCount = Animal::count();
 
+        $enclosures = [];
+
+        if (Auth::user()->admin) {
+            $enclosures = Enclosure::get();
+        } else {
+            $enclosures = Auth::user()->enclosures;
+        }
+
         $now = Carbon::now('Europe/Budapest')->format('H:i:s');
 
-/*         $feedingTasks = $enclosures->filter(function ($enclosure) use ($now) {
-            $feedingTime = Carbon::parse($enclosure->feeding_at);
-            return $feedingTime->isAfter($now);
-        });
-
-        $feedingTasks = $feedingTasks->map(function ($enclosure) {
-            $enclosure->feeding_at = $enclosure->feeding_at ? Carbon::parse($enclosure->feeding_at)->format('H:i') : 'Missing';
-            return $enclosure;
-        }); */
-
-        $feedingTasks = Auth::user()->enclosures
+        $feedingTasks = $enclosures
             ->where('feeding_at', '>', $now) // Filter feeding times after now
             ->sortBy('feeding_at') // Order enclosures by feeding_at time in ascending order
             ->map(function ($enclosure) {
@@ -43,6 +34,6 @@ class HomeController extends Controller
             });
 
 
-        return view('home', compact('title', 'enclosureCount', 'animalCount', 'feedingTasks'));
+        return view('home', compact('enclosureCount', 'animalCount', 'feedingTasks'));
     }
 }
